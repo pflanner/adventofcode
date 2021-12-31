393aebe25c09245823c657b7cd4c0de31d61342c
Peter Flanner <peter.flanner@gmail.com>
2021-12-30 22:34:03 -0800

    complete 2021 day 21

--- year2021/21.py
+++ year2021/21.py
@@ -1,6 +1,3 @@
-from functools import reduce
-
-
 def increase(p, magnitude, range):
     return (p - 1 + magnitude) % range + 1
 
@@ -33,20 +30,7 @@ def part1(p1, p2):
 def part2(player1, player2):
     mem = {}
 
-    def helper(roll, p1, p2, s1, s2, rolls, players_turn, num_turns):
-        key = (roll, p1, p2, s1, s2, players_turn, num_turns)
-        # key = tuple(rolls[:-1])
-        # if tuple(rolls) in mem:
-        #     return mem[tuple(rolls)]
-        if key in mem:
-            return mem[key]
-        if s1 >= 21:
-            # mem[key] = (1, 0)
-            return 1, 0
-        if s2 >= 21:
-            # mem[key] = (0, 1)
-            return 0, 1
-
+    def helper(roll, p1, p2, s1, s2, players_turn, num_turns):
         if players_turn == 0:
             p1 = increase(p1, roll, 10)
         else:
@@ -62,70 +46,27 @@ def part2(player1, player2):
         else:
             num_turns += 1
 
+        key = (roll, p1, p2, s1, s2, players_turn, num_turns)
+
+        if key in mem:
+            return mem[key]
+        if s1 >= 21:
+            return 1, 0
+        if s2 >= 21:
+            return 0, 1
+
         wins1 = wins2 = 0
         for r in range(1, 4):
-            w1, w2 = helper(r, p1, p2, s1, s2, rolls + [r], players_turn, num_turns)
-            key2 = (r, p1, p2, s1, s2, players_turn, num_turns)
-            # mem[key2] = w1, w2
-            # mem[tuple(rolls + [r])] = (w1, w2)
+            w1, w2 = helper(r, p1, p2, s1, s2, players_turn, num_turns)
             wins1 += w1
             wins2 += w2
 
         mem[key] = (wins1, wins2)
 
-        return wins2, wins2
+        return wins1, wins2
 
-    helper(0, player1, player2, 0, 0, [], 0, -1)
-    w1, w2 = mem[(3, 4, 8, 0, 0, 0, 0)]
-    # w1, w2 = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), mem.values())
-
-    return max(w1, w2)
-
-
-def part2_2(player1, player2):
-    mem = {}
-    score_map = {
-        3: 1,
-        4: 3,
-        5: 6,
-        6: 7,
-        7: 6,
-        8: 3,
-        9: 1,
-    }
-
-    def helper(p1, p2, s1, s2, num_dimensions, players_turn):
-        key = (p1, p2, s1, s2, players_turn)
-        if key in mem:
-            return mem[key]
-        if s1 >= 21:
-            mem[key] = (num_dimensions, 0)
-            return mem[key]
-        if s2 >= 21:
-            mem[key] = (0, num_dimensions)
-            return mem[key]
-
-        players_turn = 0 if players_turn == 1 else 1
-
-        wins1 = wins2 = 0
-        for s in range(3, 10):
-            if players_turn == 0:
-                p1 = increase(p1, s, 10)
-                s1 += p1
-            else:
-                p2 = increase(p2, s, 10)
-                s2 += p2
-            new_dimensions = num_dimensions + 3*score_map[s]
-            w1, w2 = helper(p1, p2, s1, s2, new_dimensions, players_turn)
-            wins1 += w1
-            wins2 += w2
-
-        # mem[tuple(rolls)] = (wins1, wins2)
-
-        return wins2, wins2
-
-    helper(player1, player2, 0, 0, 0, 0)
-    w1, w2 = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), mem.values())
+    initial_input = (0, player1, player2, 0, 0, 0, -1)
+    w1, w2 = helper(*initial_input)
 
     return max(w1, w2)
 
@@ -136,16 +77,5 @@ if __name__ == '__main__':
     Player 1 starting position: 6
     Player 2 starting position: 2
     """
-    # print(increase(99, 2, 100))
     print(part1(6, 2))
-    print(part2(4, 8))
-    # 262812 is not right
-    # 213246 is not right
-    """
-    4567569635410695
-    444356092776315
-    1025881170542424
-    1025881170542424
-    """
-
-
+    print(part2(6, 2))
